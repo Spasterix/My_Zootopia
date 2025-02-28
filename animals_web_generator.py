110e50ee35cf653456bcafadafe0e5b7be9bb9bf
Alexander Krause <Alexander.Krause@mail.de>
2025-02-28 20:19:53 +0100

    improve card item html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -30,18 +30,22 @@ def generate_animal_info(animal: Dict) -> str:
     Returns:
         str: HTML formatted string with animal information
     """
-    output = '<li class="cards__item">\n<div class="card__text">\n'
+    output = '<li class="cards__item">\n'
 
     if "name" in animal:
-        output += f'<div class="card__title">{animal["name"]}</div>\n'
-    if "characteristics" in animal and "diet" in animal["characteristics"]:
-        output += f'Diet: {animal["characteristics"]["diet"]}<br>\n'
-    if "locations" in animal and animal["locations"]:
-        output += f'Location: {animal["locations"][0]}<br>\n'
-    if "characteristics" in animal and "type" in animal["characteristics"]:
-        output += f'Type: {animal["characteristics"]["type"]}<br>\n'
+        output += f'    <div class="card__title">{animal["name"]}</div>\n'
 
-    output += '</div>\n</li>\n'
+    output += '    <p class="card__text">\n'
+
+    if "characteristics" in animal and "diet" in animal["characteristics"]:
+        output += f'        <strong>Diet:</strong> {animal["characteristics"]["diet"]}<br/>\n'
+    if "locations" in animal and animal["locations"]:
+        output += f'        <strong>Location:</strong> {animal["locations"][0]}<br/>\n'
+    if "characteristics" in animal and "type" in animal["characteristics"]:
+        output += f'        <strong>Type:</strong> {animal["characteristics"]["type"]}<br/>\n'
+
+    output += '    </p>\n'
+    output += '</li>\n'
     return output
 
 
